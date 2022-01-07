7cef89fc805577881cd738795fcca41a4ebdf61f
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-01-07 14:18:36 -0800

    update port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const routes = require('./routes')
 app.use(express.static(__dirname + '/benchmarks'))
 app.use('/', routes)
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
