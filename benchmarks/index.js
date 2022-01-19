1bd13c1e6df3718c8e4de88142250a46d88ebb8b
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-01-18 16:29:06 -0800

    crefactor to use html path in template

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -6,24 +6,27 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 fs.readdir(__dirname, (err, files) => {
-    files.pop()
-    files.forEach(benchmark => {
-        const scenariosPath = __dirname + `/${benchmark}/scenarios`
-        fs.readdir(scenariosPath, (err, scenarioFiles) => {
-            if (scenarioFiles) {
-                scenarioFiles.forEach((scenarioFile) => {
+    files.forEach(file => {
+        const scenariosPath = __dirname + `/${file}/scenarios`
+        const isBenchmarkFile = fs.lstatSync(`${__dirname}/${file}`).isDirectory()
+        if (isBenchmarkFile) {
+            fs.readdir(scenariosPath, (err, scenarioFiles) => {
+                scenarioFiles && scenarioFiles.forEach((scenarioFile) => {
                     const scenarioName = scenarioFile.substr(0, scenarioFile.lastIndexOf('.'))
                     const scenariocb = (req, res) => {
                         const version = req.query.releaseversion
-                        res.render(scenariosPath + `/${scenarioName}`, { version, scenarioName })
+                        res.render('index', { version, scenarioName, htmlPath: `${scenariosPath}/${scenarioName}` })
                     }
-                    router.get(`/${benchmark}/${scenarioName}`, scenariocb)
+                    router.get(`/${file}/${scenarioName}`, scenariocb)
                 })
-            }
-        })
+            })
+        }
     });
 });
+
+
+
+
 
 export default router
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import router from './benchmarks/index.js'
 const app = express();
 const port = 8080;
 
-app.set('views', '/benchmarks')
+app.set('views', './benchmarks')
 app.set('view engine', 'ejs')
 app.use('/', router)
 
