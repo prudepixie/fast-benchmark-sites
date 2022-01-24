956719c56a95c9d51b06ebd880dc306883491257
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-01-24 15:34:57 -0800

    refactor file paths

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -5,28 +5,27 @@ import fs from 'fs'
 const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const currentVersion = '1.6.2'
 
 fs.readdir(__dirname, (err, files) => {
     files.forEach(file => {
-        const scenariosPath = __dirname + `/${file}/scenarios`
+        const scenariosPath = `${__dirname}/${file}/scenarios`
         const isBenchmarkFile = fs.lstatSync(`${__dirname}/${file}`).isDirectory()
-        if (isBenchmarkFile) {
+        if (isBenchmarkFile && scenariosPath) {
             fs.readdir(scenariosPath, (err, scenarioFiles) => {
                 scenarioFiles && scenarioFiles.forEach((scenarioFile) => {
-                    const scenarioName = scenarioFile.substr(0, scenarioFile.lastIndexOf('.'))
                     const scenariocb = (req, res) => {
-                        const version = req.query.releaseversion
-                        res.render('index', { version, scenarioName, htmlPath: `${scenariosPath}/${scenarioName}` })
+                        const { type, prop } = req.query
+                        const version = req.query?.releaseversion ? req.query.releaseversion : currentVersion
+                        res.render('index', { version, htmlPath: `${scenariosPath}/${scenarioFile}/${type}/${prop}` })
                     }
-                    router.get(`/${file}/${scenarioName}`, scenariocb)
+                    router.get(`/${file}/${scenarioFile}`, scenariocb)
                 })
             })
         }
     });
 });
+
 // add error handling for wrong manual input of path
-
-
-
 
 export default router
